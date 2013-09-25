5a4c98d3e51251f6a0df2789bdee25aad3b26b62
Jarmo Isotalo <jamo@isotalo.fi>
2013-09-25 22:44:23 +0300

    asd

---
+++ week1_5_7_SimpleMath/src/main.c
@@ -0,0 +1,6 @@
+#include "mathfuncs.h"
+
+int main()
+{
+	return 0;
+}
---
+++ week1_5_7_SimpleMath/src/mathfuncs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "mathfuncs.h"
+
+int gcd_iterative(int a, int b) {
+	/* BEGIN SOLUTION */
+	return b/a;
+	/* END SOLUTION */
+	/* STUB: return 0; */
+}
