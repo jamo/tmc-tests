74068742a193eaf8a6815f282ad5dd6d1e2beae2
Jarmo Isotalo <jamo@isotalo.fi>
2013-09-25 23:21:14 +0300

    aikoja

--- week1_5_7_SimpleMath/test/test_mathfuncs.c
+++ week1_5_7_SimpleMath/test/test_mathfuncs.c
@@ -13,20 +13,54 @@ START_TEST(test_gcd_iterative)
   int *a = malloc(sizeof(unsigned long long int)* TIMES );
   unsigned long long int b = 0;
   while (b < TIMES) {
-    if (b % 10000 == 0){
-      printf("%llu\n", b);
-    }
     a[b] = b;
     b++;
   }
 	fail_unless(gcd_iterative(15, 20) == 5, "gcd_iterative(15, 20) should return 5");
 }
+START_TEST(test_gcd_iterative2)
+{
+  int *a = malloc(sizeof(unsigned long long int)* TIMES );
+  unsigned long long int b = 0;
+  while (b < TIMES) {
+    a[b] = b;
+    b++;
+  }
+  fail_unless(gcd_iterative(15, 20) == 5, "gcd_iterative(15, 20) should return 5");
+}
 END_TEST
+
+
+START_TEST(test_gcd_iterative3)
+{
+  int *a = malloc(sizeof(unsigned long long int)* TIMES );
+  unsigned long long int b = 0;
+  while (b < TIMES) {
+    a[b] = b;
+    b++;
+  }
+  fail_unless(gcd_iterative(15, 20) == 5, "gcd_iterative(15, 20) should return 5");
+}
+
+START_TEST(test_gcd_iterative4)
+{
+  int *a = malloc(sizeof(unsigned long long int)* TIMES );
+  unsigned long long int b = 0;
+  while (b < TIMES) {
+
+    a[b] = b;
+    b++;
+  }
+  fail_unless(gcd_iterative(15, 20) == 5, "gcd_iterative(15, 20) should return 5");
+}
 
 int main(int argc, const char *argv[])
 {
 	Suite *s = suite_create("Test");
-	tmc_register_test(s, test_gcd_iterative, "7.0");
+  tmc_register_test(s, test_gcd_iterative, "7.0");
+  tmc_register_test(s, test_gcd_iterative2, "7.0");
+  tmc_register_test(s, test_gcd_iterative3, "7.0");
+  tmc_register_test(s, test_gcd_iterative4, "7.0");
 
 	return tmc_run_tests(argc, argv, s);
 }
